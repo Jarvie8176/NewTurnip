83cb2f6bae5bdaa23500d9eeb30b322d655970c7
JarvieK <kongjw1993@gmail.com>
2020-04-05 01:15:08 -0400

    add qq group link

--- packages/ui/src/shared/config.ts
+++ packages/ui/src/shared/config.ts
@@ -6,4 +6,5 @@ export const Config = {
   API_URL: decodeDto(string, process.env.REACT_APP_API_URL),
   GA_TRACKER_TAG: decodeDto(string, process.env.REACT_APP_GA_TRACKER_TAG),
   SLACK_WORKSPACE_LINK: decodeDto(string, process.env.REACT_APP_SLACK_WORKSPACE_LINK),
+  QQ_GROUP_LINK: decodeDto(string, process.env.REACT_APP_QQ_GROUP_LINK),
 };
